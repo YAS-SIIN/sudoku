5b7b91b746f085c6292683b8db10076d746cc694
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-09-13 15:57:31 +0330

    Orginized components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+export const routes: Routes = [
+    {
+      path: '',
+      loadComponent: () =>
+        import('./components/layout/layout.component').then(
+          (c) => c.LayoutComponent
+        )
 
-export const routes: Routes = [];
+    },
+
+  ];
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NgIf } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ContactLinkComponent } from "../contact-link/contact-link.component";
+import { SudokuBoardComponent } from '../sudoku-board/sudoku-board.component';
+import { ControlPanelComponent } from '../control-panel/control-panel.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, NgIf, RouterOutlet, ContactLinkComponent, SudokuBoardComponent, ControlPanelComponent  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
